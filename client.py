92ea0396efe2d1da0e6d895cf70eb712696f1aba
Lautaro Russo <lautaro.russo@estudiants.urv.cat>
2021-02-27 23:37:12 -0300

    client cli falta inplementar metodos

--- client.py
+++ client.py
@@ -1,34 +1,40 @@
 import click
+from click.decorators import argument
 @click.group()
 def cli():
     pass
 
-@cli.command('create')
+@cli.command('create',help='Creates new work node.')
 def workerCreate():
     click.echo('worker create')
     return 0
 
-@click.command('delete')
+@click.command('delete',help='Deletes work node by given index.')
 @click.argument('num')
 def workerDelete(num):
-    click.echo('worker delete {}', num)
+    click.echo(num)
     return 0
-cli.add_command(workerDelete)
 
-@cli.command('list')
+@cli.command('list', help='List all work nodes.')
 def workerList():
     click.echo('worker list')
     return 0
 
-@cli.command()
-def wordCount(num, files):
+@click.command('count', help='Count number of words of a text file by given url.')
+@click.argument('files')
+def wordCount(files):
     click.echo('word count method')
     return 0
 
-@cli.command()
-def countWords(num, files):
+@click.command('enumerate', help='Enumerate repeated words in a text file by given url.')
+@click.argument('files')
+def countWords(files):
     click.echo('count words method')
     return 0
+
+cli.add_command(wordCount)
+cli.add_command(countWords)
+cli.add_command(workerDelete)
 
 if __name__ == '__main__':
     cli()
